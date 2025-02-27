aca0c1ea4b44794a44569524226fedf654748f83
gaimn <104718139+gaimn@users.noreply.github.com>
2025-02-27 22:23:10 +0000

    anti-bouncespam #3, no total bounce abusing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Hi")
 let fps = 60
 let x = 0; let y = 0
 let xv = 12; let yv = 12
-let bounce_time = 0; let total_bounces = 0
+let bounce_time = 0; let bouncer = false; let total_bounces = 0
 
 function move() {
     x += xv; y += yv
@@ -12,11 +12,11 @@ function move() {
     var ya = y > innerHeight - 200; var yb = y < 0
 
     if (xa || xb) {
-        xv *= -1; bounce_time = 0; total_bounces++
+        xv *= -1; bounce_time = 0; bouncer++; total_bounces++
         document.getElementById("count").innerHTML = total_bounces + " Boing(s)"
     }
     if (ya || yb) {
-        yv *= -1; bounce_time = 0; total_bounces++
+        yv *= -1; bounce_time = 0; bouncer++; total_bounces++
         document.getElementById("count").innerHTML = total_bounces + " Boing(s)"
     }
 
@@ -24,12 +24,19 @@ function move() {
     document.getElementById("boing").style.left = x + "px"
     document.getElementById("boing").style.top = y + "px"
 
-    if (bounce_time < 3) {
-        x = 0; y = 0; xv = 12; yv = 12; bounce_time = 0
+    if (bounce_time < 3 && bouncer > 2) {
+        x = 0; y = 0; xv = 12; yv = 12; bounce_time = 0; bouncer = 0
+        total_bounces -= 4
+        document.getElementById("count").innerHTML = total_bounces + " Boing(s)"
+        document.getElementById("why").hidden = false
+        setTimeout(() => {
+            document.getElementById("why").hidden = true
+        }, 3000)
     }
     //bounces = bounces >= 0.5 ? bounces - 0.5 : bounces
-    bounce_time++
+    bounce_time++; bouncer = bouncer > 0 ? bouncer - 0.5 : bouncer
 }
 
 setInterval(move, 1e3 / fps)
 // UPDATE WEB
+// 42
