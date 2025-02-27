b6fa421d31485a268afe25ce877e3ce88c1ff2e6
gaimn <104718139+gaimn@users.noreply.github.com>
2025-02-27 22:17:24 +0000

    bounce rework (you can't abuse it for more boings now)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Hi")
 let fps = 60
 let x = 0; let y = 0
 let xv = 12; let yv = 12
-let bounces = 0; let total_bounces = 0
+let bounce_time = 0; let total_bounces = 0
 
 function move() {
     x += xv; y += yv
@@ -12,11 +12,11 @@ function move() {
     var ya = y > innerHeight - 200; var yb = y < 0
 
     if (xa || xb) {
-        xv *= -1; bounces++; total_bounces++
+        xv *= -1; bounce_time = 0; total_bounces++
         document.getElementById("count").innerHTML = total_bounces + " Boing(s)"
     }
     if (ya || yb) {
-        yv *= -1; bounces++; total_bounces++
+        yv *= -1; bounce_time = 0; total_bounces++
         document.getElementById("count").innerHTML = total_bounces + " Boing(s)"
     }
 
@@ -24,10 +24,11 @@ function move() {
     document.getElementById("boing").style.left = x + "px"
     document.getElementById("boing").style.top = y + "px"
 
-    if (bounces > 10) {
-        x = 0; y = 0; xv = 12; yv = 12; bounces = 0
+    if (bounce_time < 3) {
+        x = 0; y = 0; xv = 12; yv = 12; bounce_time = 0
     }
-    bounces = bounces >= 0.5 ? bounces - 0.5 : bounces
+    //bounces = bounces >= 0.5 ? bounces - 0.5 : bounces
+    bounce_time++
 }
 
 setInterval(move, 1e3 / fps)
