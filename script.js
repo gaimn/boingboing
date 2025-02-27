1dc56ec29638384023982ff0214272b09b47e7d2
gaimn <104718139+gaimn@users.noreply.github.com>
2025-02-27 18:38:09 +0000

    anti-bouncespam
    noclipping into the backrooms is ILLEGAL!!!!!!!!

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let fps = 60
 let x = 0; let y = 0
 let xv = 12; let yv = 12
+let bounces = 0
 
 function move() {
     x += xv; y += yv
-    xv = x > innerWidth - 200 ? -xv : xv; yv = y > innerHeight - 200 ? -yv : yv
-    xv = x < 0 ? -xv : xv; yv = y < 0 ? -yv : yv
+
+    var xa = x > innerWidth - 200; var xb = x < 0
+    var ya = y > innerHeight - 200; var yb = y < 0
+
+    if (xa || xb) {
+        xv *= -1; bounces++
+    }
+    if (ya || yb) {
+        yv *= -1; bounces++
+    }
 
     // a variable wouldn't work for whatever damn reason
     document.getElementById("boing").style.left = x + "px"
     document.getElementById("boing").style.top = y + "px"
+
+    if (bounces > 10) {
+        x = 0; y = 0; xv = 12; yv = 12; bounces = 0
+    }
+    bounces -= 0.5
 }
 
 setInterval(move, 1e3 / fps)
